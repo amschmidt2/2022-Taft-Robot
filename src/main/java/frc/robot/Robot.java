f04d7d6761cfd9745749d2076088d175dfd29191
Andrew Wingate <1464801+anfroholic@users.noreply.github.com>
2022-01-17 17:39:16 -0600

    import to 2022 and edge detection for dump truck

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -26,19 +26,68 @@ public class Robot extends TimedRobot {
   private CANSparkMax front_RightyMotor = new CANSparkMax(3, MotorType.kBrushless);
   private CANSparkMax back_RightyMotor = new CANSparkMax(4, MotorType.kBrushless);
 
+  SpeedControllerGroup m_lefty = new SpeedControllerGroup(front_LeftyMotor, back_LeftyMotor);
+  SpeedControllerGroup m_righty = new SpeedControllerGroup(front_RightyMotor, back_RightyMotor);
+
+
   //Joysticks
   private Joystick joy0 = new Joystick(0);
   private Joystick joy1 = new Joystick(1);
 
   //Drivechain
-  
-  private DifferentialDrive drivechain = new DifferentialDrive(leftMotor, rightMotor);
+
+  private DifferentialDrive drivechain = new DifferentialDrive(m_lefty, m_righty);
+
+  Room room =  new Room();
 
   @Override
-  public void robotInit() {}
+  public void robotInit() {
+    
+    
+
+
+  }
 
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+
+    // Xbox A, B, Y, X, tiny two frames (TTF.), tiny three line(T3L), Dpad
+    // Right Bummper, Left Bummper, Right Trigger, Left Trigger, Top Joystick, Bottom Joystick
+    // This was the shooter speed these are not things to look at buttons wise.
+    boolean xbox_A = joy0.getRawButton(1); // xbox A
+    boolean xbox_B = joy0.getRawButton(2); // xbox B
+    boolean xbox_X = joy0.getRawButton(3); // xbox X
+    boolean xbox_Y = joy0.getRawButton(4); // xbox Y
+
+    
+    elevator(xbox_A, xbox_B);
+
+    double joy_Left = joy0.getRawAxis(1);
+    double joy_Right = joy0.getRawAxis(0);
+
+    driveTruck(joy_Left, joy_Right);
+
+    
+    // checking if the button has changed
+    if (xbox_X != room.dumpTruck_prev_but) {
+      
+      //if state is true make false (this is a if, else statement within a if statement)
+      if(room.dumpTruck_state) {
+        room.dumpTruck_state = false;
+      }
+      //else make state true (this else relates to the second true)
+      else{
+        room.dumpTruck_state = true;
+      }
+    }
+    else{}
+
+    dumpTruck();
+
+    //controller, button
+    //some holder variable to track state
+
+   }
 
   @Override
   public void autonomousInit() {}
@@ -49,8 +98,6 @@ public class Robot extends TimedRobot {
 //   @Override
 // // drivechain.arcadeDrive(speed, turn);
 // // System.out.println("arcade drive speed: "+speed+", turn: "+turn);
-
-
 
 // //   }
 
@@ -65,4 +112,55 @@ public class Robot extends TimedRobot {
 
   @Override
   public void testPeriodic() {}
+ // }
+
+// hii
+
+private void dumpTruck() {
+
+  if (room.dumpTruck_state) {
+    System.out.println("dumpTruck moves up");
+  }
+
+  else {
+    System.out.println("dumpTruck moves down");
+  }
+
 }
+
+
+
+private void elevator(boolean up, boolean down) {
+  if (up){
+    System.out.println("Elevator moves up");
+  }
+
+  else if(down){
+    System.out.println("Elevator moves down");
+  }
+
+  else{
+    System.out.println("Elevator stops");
+  }
+
+}
+
+
+private void driveTruck(double speed, double turn_raw){
+
+  double turn = Math.pow(turn_raw, 2.0);
+  if (turn_raw < 0){
+    turn = -turn;
+  }
+
+  //drive train control
+  drivechain.arcadeDrive(speed, turn);
+  System.out.println("arcade drive speed: " + speed + ", turn: " + turn);
+}
+
+public class Room{
+    boolean dumpTruck_state = false;
+    boolean dumpTruck_prev_but = false;
+  }
+
+}  // <--- Leave this close brace
