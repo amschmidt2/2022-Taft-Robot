3ef2ad748378e712240e987f7843c7280b3ad49c
Andrew Wingate <1464801+anfroholic@users.noreply.github.com>
2022-01-24 18:02:15 -0600

    created a chaos party room

--- 2022-Taft/src/main/java/frc/robot/Robot.java
+++ 2022-Taft/src/main/java/frc/robot/Robot.java
@@ -33,8 +33,8 @@ public class Robot extends TimedRobot {
 
   private final DifferentialDrive drivechain = new DifferentialDrive(louie, rodger);
   
-  private final XboxController joy0 = new XboxController(0);
-  private final XboxController joy1 = new XboxController(1);
+  // private final XboxController joy0 = new XboxController(0);
+  // private final XboxController joy1 = new XboxController(1);
 
 
   private static final String kDefaultAuto = "Default";
@@ -75,12 +75,12 @@ public class Robot extends TimedRobot {
   Timer timmy = new Timer();
   SpyLord archie = new SpyLord("archie");
   Wheels wally = new Wheels("wally", .7);
-  BallHandler bobby = new BallHandler("bobby");
+  CargoChief bobby = new CargoChief("bobby");
   Intake izzy = new Intake("izzy");
   Conveyor conner = new Conveyor("conner");
   Shooter sunny = new Shooter("sunny");
-  Controller driver = new Controller("driver");
-  Controller gunner = new Controller("gunner");
+  Driver driver = new Driver("driver", 0);
+  Gunner gunner = new Gunner("gunner", 1);
   
  
   @Override
@@ -148,92 +148,142 @@ public class Robot extends TimedRobot {
     // Xbox A, B, Y, X, tiny two frames (TTF.), tiny three line(T3L), Dpad
     // Right Bummper, Left Bummper, Right Trigger, Left Trigger, Top Joystick, Bottom Joystick
     // This was the shooter speed these are not things to look at buttons wise.
-    boolean xbox_A = joy0.getRawButton(1); // xbox A
-    boolean xbox_B = joy0.getRawButton(2); // xbox B
-    boolean xbox_X = joy0.getRawButton(3); // xbox X
-    boolean xbox_Y = joy0.getRawButton(4); // xbox Y
+    // boolean xbox_A = joy0.getRawButton(1); // xbox A
+    // boolean xbox_B = joy0.getRawButton(2); // xbox B
+    // boolean xbox_X = driver.get_but(3); // xbox X
+    // boolean xbox_Y = driver.get_but(4); // xbox Y
 
-    dumpTruck(xbox_X, xbox_Y);
-    climber(xbox_A, xbox_B);
+    dumpTruck(driver.get_but(3), driver.get_but(4));
+    // climber(xbox_A, xbox_B);
 
-    double joy_Left = joy0.getRawAxis(1);
-    double joy_Right = joy0.getRawAxis(0);
+    //double joy_Left = driver.get_axis(1);
+    //double joy_Right = driver.get_axis(0);
 
    
-    wally.check(joy_Left, joy_Right);
+    wally.check(driver.get_axis(1), driver.get_axis(0));
+    // bobby.check();
+    //wally.check(left=driver.get_axis('x'), right=driver.get_axis('y'))
     
   }
 
-public class Controller{
-  private String name;
+  public class Driver{
+    private String name;
+    private XboxController joy;
 
-  public Controller(String _name){
-     name = _name;
-     System.out.println(name + " coming in hot ");
+    public Driver(String _name, int port_num){
+        name = _name;
+        joy = new XboxController(port_num);
+        System.out.println(name + " coming in hot ");
+      }
+
+      public void talk(){
+        System.out.println(" Hi, I'm " + name + " I am the one who drives, Kachow! ");
+      } 
+
+      public boolean get_but(int but_num){
+        return joy.getRawButton(but_num);
+      }
+      public double get_axis(int raw_axis){
+        return joy.getRawAxis(raw_axis);
+      }
     }
 
-    public void talk(){
-     System.out.println(" Hi, I'm " + name + " I am a controller, woah! ");
-    } 
-  }
+  public class Gunner{
+    private String name;
+    private XboxController joy;
 
+    public Gunner(String _name, int port_num){
+      name = _name;
+      joy = new XboxController(port_num);
+      System.out.println(name + " coming in hot ");
+      }
 
-
+      public void talk(){
+      System.out.println(" Hi, I'm " + name + " I work with the weapons, pew pew! ");
+      } 
+      public boolean get_but(int but_num){
+        return joy.getRawButton(but_num);
+      }
+      public double get_axis(int raw_axis){
+        return joy.getRawAxis(raw_axis);
+      }
+    }
 
 
   
-  public class BallHandler{
+  public class CargoChief{
     private String name;
-
-    public BallHandler(String _name){
-     name = _name;
-     System.out.println(name + " better bob is here ");
+    boolean wants_cargo[] = {false, false};
+    String state = "idle";
+    
+    public CargoChief(String _name){
+      name = _name;
+      System.out.println(name + " better bob is here ");
     }
 
-    public void talk(){
-     System.out.println(" Hi, I'm " + name + " I tell you knowledge about cargo");
+    public void talk(){      
+      System.out.println(" Hi, I'm " + name + " I tell you knowledge about cargo");
     } 
+    public void check(){
+      boolean ahhh [] = {false, false};
+      ahhh = event_chk(driver.get_but(1), wants_cargo[1]);
+      if (ahhh[0]){
+        
+        if (ahhh[1]){
+          System.out.println("move all this to some other function make code clean like below");
+        }
+
+      }
+      // This is the code I want to see in bobby's maing check function
+      // if(wants_to_fire()){
+      //   state = "firing";
+      // }
+      // else if(wants_cargo()){
+      //   state = "get_cargo";
+      // }
+      
+    }
   }
 
 
-public class Intake{
-    private String name;
+  public class Intake{
+      private String name;
 
-    public Intake(String _name){
-     name = _name;
-     System.out.println(name + " izzy is on the scene ");
+      public Intake(String _name){
+      name = _name;
+      System.out.println(name + " izzy is on the scene ");
+      }
+
+      public void talk(){
+      System.out.println(" Hi, I'm " + name + " I reach out and collect cargo");
+      } 
     }
-
-    public void talk(){
-     System.out.println(" Hi, I'm " + name + " I reach out and collect cargo");
-    } 
-  }
 
   public class Conveyor{
     private String name;
 
     public Conveyor(String _name){
-     name = _name;
-     System.out.println(name + " has rolled in ");
+      name = _name;
+      System.out.println(name + " has rolled in ");
     }
 
     public void talk(){
-     System.out.println(" Hi, I'm " + name + " I move the cargo ");
+      System.out.println(" Hi, I'm " + name + " I move the cargo ");
     } 
   }
 
- public class Shooter{
-    private String name;
+    public class Shooter{
+      private String name;
 
-    public Shooter(String _name){
-     name = _name;
-     System.out.println(name + " i am very happy ");
+      public Shooter(String _name){
+        name = _name;
+        System.out.println(name + " i am very happy ");
+      }
+
+      public void talk(){
+        System.out.println(" Hi, I'm " + name + " I get rid of cargo, pew pew, yuck!");
+      } 
     }
-
-    public void talk(){
-     System.out.println(" Hi, I'm " + name + " I get rid of cargo, pew pew, yuck!");
-    } 
-  }
 
 
   public class Wheels{
@@ -256,9 +306,9 @@ public class Intake{
 
 
     private void drive(double speed, double turn){
-     drivechain.arcadeDrive(speed, turn);
-   }
-   
+      drivechain.arcadeDrive(speed, turn);
+    }
+
     private void sensitive(double speed, double turn){
       turn = Math.pow(turn, 2.0);
       if (turn < 0){
@@ -268,103 +318,114 @@ public class Intake{
 
       drive(speed, turn);
     } 
-
   } 
 
-  public class SpyLord {
-    private String spyroom [][]= {
-        {"Back move", "Stop move", "2.0"},
-        {"Forward move", "Stop move", "2.5"},
-        {"Shoot Cargo", "Stay off", "4.5"},
-        {"Back move", "Stop move", "5.0"}, 
-        {"Done", "this will never run", "999.9"}
-      };
-    private int autonomous_counter = 0;
-    private double lil_sam = 0;
-    private String name;
 
-    
-    public SpyLord(String _name){
-      name = _name;
-      System.out.println(name + " has entered ");
-    }
+public class SpyLord {
+  private String spyroom [][]= {
+      {"Back move", "Stop move", "2.0"},
+      {"Forward move", "Stop move", "2.5"},
+      {"Shoot Cargo", "Stay off", "4.5"},
+      {"Back move", "Stop move", "5.0"}, 
+      {"Done", "this will never run", "999.9"}
+    };
+  private int autonomous_counter = 0;
+  private double lil_sam = 0;
+  private String name;
 
-    
-    public void start(){
-      timmy.reset();
-      timmy.start();
-      briefcase(spyroom[0][0]);
-      lil_sam = lil_sam + Double.parseDouble(spyroom[0][2]); 
-    }
-    
-    
-    public void check(){
-      if(timmy.get() > lil_sam){  // <-- This is an event
-        briefcase(spyroom[autonomous_counter][1]);      
-        autonomous_counter++;
-        briefcase(spyroom[autonomous_counter][0]);
-        lil_sam = lil_sam + Double.parseDouble(spyroom[autonomous_counter][2]); 
-        
-      }
-    }
-    
-    
-    private void briefcase(String task){
-      System.out.println(task);
-      switch(task){
-        case "Back move":
-          System.out.println("The wheels are moving back!");
-          // drivechain.arcadeDrive(speed, turn);
-        default:
-          break;
-      }
-    }
-
-    public void talk(){
-      System.out.println( " Hi, I'm " + name + " I am the boss of autonomous, you can look at tasks in the spyroom and make sure there is a function for it in the briefcase");
-    }
-
+  
+  public SpyLord(String _name){
+    name = _name;
+    System.out.println(name + " has entered ");
   }
 
-
-
-
-  private void climber(boolean up, boolean down) {
-    if (up){
-      System.out.println("Elevator moves up");
-    }
   
-    else if(down){
-      System.out.println("Elevator moves down");
-    }
-  
-    else{
-      System.out.println("Elevator stops");
-    }
-  
+  public void start(){
+    timmy.reset();
+    timmy.start();
+    briefcase(spyroom[0][0]);
+    lil_sam = lil_sam + Double.parseDouble(spyroom[0][2]); 
   }
-  private void dumpTruck(boolean up, boolean down) {
-
-    if (up) {
-      System.out.println("dumpTruck moves up");
-    }
   
-    else if (down) {
-      System.out.println("dumpTruck moves up");
-    }
   
-    else {
-      System.out.println("dumpTruck off");
+  public void check(){
+    if(timmy.get() > lil_sam){  // <-- This is an event
+      briefcase(spyroom[autonomous_counter][1]);      
+      autonomous_counter++;
+      briefcase(spyroom[autonomous_counter][0]);
+      lil_sam = lil_sam + Double.parseDouble(spyroom[autonomous_counter][2]); 
+      
     }
+  }
   
+  
+  private void briefcase(String task){
+    System.out.println(task);
+    switch(task){
+      case "Back move":
+        System.out.println("The wheels are moving back!");
+        // drivechain.arcadeDrive(speed, turn);
+      default:
+        break;
+    }
   }
 
+  public void talk(){
+    System.out.println( " Hi, I'm " + name + " I am the boss of autonomous, you can look at tasks in the spyroom and make sure there is a function for it in the briefcase");
+  }
+
+}
+
+
+
+
+private void climber(boolean up, boolean down) {
+  if (up){
+    System.out.println("Elevator moves up");
+  }
+
+  else if(down){
+    System.out.println("Elevator moves down");
+  }
+
+  else{
+    System.out.println("Elevator stops");
+  }
+
+}
+
+private void dumpTruck(boolean up, boolean down) {
+
+  if (up) {
+    System.out.println("dumpTruck moves up");
+  }
+
+  else if (down) {
+    System.out.println("dumpTruck moves up");
+  }
+
+  else {
+    System.out.println("dumpTruck off");
+  }
+
+}
+
+  boolean[] event_chk(boolean now, boolean past){
+    boolean partyroom[] = {false, false};
+    if (now != past){
+      partyroom[0] = true;
+        if(!past){
+          partyroom[1] = true;
+        }
+    }
+    return partyroom;
+  }
 
   @Override
   public void disabledInit() {
     // Stop the rumble when entering disabled
-    joy0.setRumble(RumbleType.kLeftRumble, 0.0);
-    joy0.setRumble(RumbleType.kRightRumble, 0.0);
+    // joy0.setRumble(RumbleType.kLeftRumble, 0.0);
+    // joy0.setRumble(RumbleType.kRightRumble, 0.0);
   }
   @Override
   public void disabledPeriodic() {}
